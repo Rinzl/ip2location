73b1b6e974c43123fc9cd21f7376d713194d6416
Dangth <tranhaidang2320@gmail.com>
2019-08-19 00:54:45 +0700

    json to object

--- app/src/main/java/com/dangth/ip2location/services/IpApi.java
+++ app/src/main/java/com/dangth/ip2location/services/IpApi.java
@@ -1,4 +1,11 @@
 package com.dangth.ip2location.services;
 
+import com.dangth.ip2location.services.model.IpInfoResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface IpApi {
+    @GET
+    Call<IpInfoResponse> getIpInfo();
 }
--- app/src/main/java/com/dangth/ip2location/services/model/IpInfoResponse.java
+++ app/src/main/java/com/dangth/ip2location/services/model/IpInfoResponse.java
@@ -1,4 +1,52 @@
 package com.dangth.ip2location.services.model;
 
-public class IpInfoResponse {
+import java.io.Serializable;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class IpInfoResponse implements Serializable {
+    private final static long serialVersionUID = -2076024437610846467L;
+
+    @SerializedName("as")
+    @Expose
+    public String as;
+    @SerializedName("city")
+    @Expose
+    public String city;
+    @SerializedName("country")
+    @Expose
+    public String country;
+    @SerializedName("countryCode")
+    @Expose
+    public String countryCode;
+    @SerializedName("isp")
+    @Expose
+    public String isp;
+    @SerializedName("lat")
+    @Expose
+    public double lat;
+    @SerializedName("lon")
+    @Expose
+    public double lon;
+    @SerializedName("org")
+    @Expose
+    public String org;
+    @SerializedName("query")
+    @Expose
+    public String query;
+    @SerializedName("region")
+    @Expose
+    public String region;
+    @SerializedName("regionName")
+    @Expose
+    public String regionName;
+    @SerializedName("status")
+    @Expose
+    public String status;
+    @SerializedName("timezone")
+    @Expose
+    public String timezone;
+    @SerializedName("zip")
+    @Expose
+    public String zip;
 }
